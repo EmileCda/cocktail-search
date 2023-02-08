938d702c340c0280e7269069c1ea67093a6069da
EmileCda <emile.Cda@gmail.com>
2023-02-08 17:13:52 +0100

    bug tabulation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // javascript for coctail search project
+
+
+// ------------------------------------------------------------------------------------------------------
+/**
+ * what to do when click on the logo : add the searchbar on the page 
+ *  and resize logo (half of splash)
+ * 
+ */
+function clickLogo(){
+	
+}
+// ------------------------------------------------------------------------------------------------------
+function initPage(){
+
+	addMainContainer()
+	setScreenSize();
+	window.onresize = setScreenSize; // if windows resize then resize mainContainer
+
+	addDivSearchAndLogo()
+	addLogo()
+	document.querySelector('.logo')
+	        .addEventListener('click', clickLogo)			// if click on the splash then change page to logo + search bar
+	 
+}
+
+
+document.addEventListener("DOMContentLoaded",initPage); 	// waiting for document loaded before code executing
